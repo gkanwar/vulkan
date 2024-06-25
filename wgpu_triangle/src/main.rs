c3442967b60e49731908fe6ec4804c6114423655
Gurtej Kanwar <gurtej@mit.edu>
2024-06-25 20:19:32 +0200

    wgpu: basic clear screen

--- wgpu_triangle/src/main.rs
+++ wgpu_triangle/src/main.rs
@@ -4,9 +4,10 @@ use winit::{
   event::*,
   event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
   window::{Window, WindowId},
-  dpi::PhysicalSize,
 };
 use pollster::FutureExt as _;
+
+type Size = winit::dpi::PhysicalSize<u32>;
 
 struct State {
   window: Arc<Window>,
@@ -14,12 +15,66 @@ struct State {
   instance: wgpu::Instance,
   config: wgpu::SurfaceConfiguration,
   queue: wgpu::Queue,
-  size: PhysicalSize<u32>,
+  device: wgpu::Device,
+  size: Size,
 }
 
 #[derive(Default)]
 struct App {
   state: Option<State>,
+}
+
+impl App {
+  pub fn resize(&mut self, new_size: Size) {
+    if new_size.width == 0 || new_size.height == 0 {
+      return;
+    }
+    if let Some(state) = &mut self.state {
+      state.size = new_size;
+      state.config.width = state.size.width;
+      state.config.height = state.size.height;
+      state.surface.configure(&state.device, &state.config);
+    }
+  }
+  pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+    let state = match &mut self.state {
+      Some(state) => state,
+      None => {
+        return Ok(());
+      }
+    };
+    let output = state.surface.get_current_texture()?;
+    let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+    let enc_desc = wgpu::CommandEncoderDescriptor {
+      label: Some("render encoder"),
+    };
+    let mut encoder = state.device.create_command_encoder(&enc_desc);
+    {
+      let color_attach = wgpu::RenderPassColorAttachment {
+        view: &view,
+        resolve_target: None,
+        ops: wgpu::Operations {
+          load: wgpu::LoadOp::Clear(wgpu::Color {
+            r: 0.1, g: 0.1, b: 0.1, a: 1.0,
+          }),
+          store: wgpu::StoreOp::Store,
+        }
+      };
+      let rp_desc = wgpu::RenderPassDescriptor {
+        label: Some("render pass"),
+        color_attachments: &[Some(color_attach)],
+        depth_stencil_attachment: None,
+        occlusion_query_set: None,
+        timestamp_writes: None,
+      };
+      let render_pass = encoder.begin_render_pass(&rp_desc);
+    }
+
+    state.queue.submit(std::iter::once(encoder.finish()));
+    output.present();
+
+    Ok(())
+  }
 }
 
 impl ApplicationHandler for App {
@@ -75,31 +130,52 @@ impl ApplicationHandler for App {
       config,
       surface,
       window,
+      device,
     });
   }
 
   fn window_event(
     &mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent,
   ) {
-    if let Some(state) = &self.state {
-      // not our window
-      if state.window.id() != id {
-        return;
-      }
-      match event {
-        WindowEvent::CloseRequested => {
-          event_loop.exit();
-        },
-        WindowEvent::RedrawRequested => {
-          state.window.request_redraw();
-        },
-        _ => {}
-      }
-    }
-    else {
-      // no state, i.e. not initialized
+    let state = match &self.state {
+      Some(state) => state,
+      None => return,
+    };
+    // not our window
+    if state.window.id() != id {
       return;
     }
+    let size = state.size;
+
+    match event {
+      WindowEvent::CloseRequested => {
+        event_loop.exit();
+      },
+      WindowEvent::RedrawRequested => {
+        state.window.request_redraw();
+        let res = self.render();
+        match res {
+          Ok(_) => {},
+          // surface needs to be reconfigured
+          Err(wgpu::SurfaceError::Lost) => {
+            self.resize(size);
+          },
+          // no memory, abort
+          Err(wgpu::SurfaceError::OutOfMemory) => {
+            event_loop.exit();
+          },
+          // ignore other errors
+          Err(e) => {
+            eprintln!("{:?}", e);
+          },
+        };
+      },
+      WindowEvent::Resized(size) => {
+        self.resize(size);
+      },
+      _ => {}
+    }
+
   }
 }
 
