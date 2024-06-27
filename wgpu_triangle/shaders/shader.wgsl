85ba1cd4e7acc87321353c5d750bcc4cda0e746e
Gurtej Kanwar <gurtej@mit.edu>
2024-06-27 22:48:10 +0200

    wgpu: mesh with vertex, color, and index buffers
    push constants still not working on Mac

--- wgpu_triangle/shaders/shader.wgsl
+++ wgpu_triangle/shaders/shader.wgsl
@@ -1,23 +1,32 @@
-struct VertexOutput {
-  // output to rasterizer
-  @builtin(position) clip_position: vec4<f32>,
-  // pass forward to frag shader
-  @location(0) vert_pos: vec3<f32>,
+struct VertexInput {
+  @location(0) pos: vec3<f32>,
+  @location(1) color: vec3<f32>,
 };
 
+struct VertexOutput {
+  @builtin(position) clip_position: vec4<f32>,
+  @location(0) color: vec3<f32>,
+};
+
+struct PushConstants {
+  model: mat4x4<f32>,
+  view: mat4x4<f32>,
+  proj: mat4x4<f32>,
+};
+var<push_constant> c: PushConstants;
+
 @vertex
-fn vs_main(@builtin(vertex_index) in_vertex_index: u32)
+fn vs_main(in: VertexInput)
     -> VertexOutput {
   var out: VertexOutput;
-  // procedural triangle coordinates
-  let x = f32(1 - i32(in_vertex_index)) * 0.5;
-  let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
-  out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
-  out.vert_pos = out.clip_position.xyz;
+  out.color = in.color;
+  var pos: vec4<f32> = vec4<f32>(in.pos, 1.0);
+  out.clip_position = pos * c.model * c.view * c.proj;
   return out;
 }
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-  return vec4<f32>(in.vert_pos.x, in.vert_pos.y, 0.0, 1.0);
+  // return vec4<f32>(in.vert_pos.x, in.vert_pos.y, 0.0, 1.0);
+  return vec4<f32>(in.color, 1.0);
 }
--- wgpu_triangle/src/main.rs
+++ wgpu_triangle/src/main.rs
@@ -5,11 +5,165 @@ use winit::{
   event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
   window::{Window, WindowId},
 };
+use wgpu::util::DeviceExt;
 use pollster::FutureExt as _;
+use bytemuck::{
+  Pod, Zeroable,
+};
+use glam::{
+  Vec3, Mat4, Quat,
+};
 
 type Size = winit::dpi::PhysicalSize<u32>;
 
-struct State {
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Pod, Zeroable)]
+struct VertexCore {
+  pos: [f32; 3],
+}
+impl VertexCore {
+  pub fn new(pos: [f32; 3]) -> Self {
+    Self { pos }
+  }
+}
+impl From<[f32; 3]> for VertexCore {
+  fn from(pos: [f32; 3]) -> Self {
+    Self { pos }
+  }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Pod, Zeroable)]
+struct VertexAux {
+  color: [f32; 3],
+}
+impl VertexAux {
+  pub fn new(color: [f32; 3]) -> Self {
+    Self { color }
+  }
+}
+
+const VERT_CORE_LAYOUT: wgpu::VertexBufferLayout  =
+  wgpu::VertexBufferLayout {
+    array_stride: std::mem::size_of::<VertexCore>() as wgpu::BufferAddress,
+    step_mode: wgpu::VertexStepMode::Vertex,
+    attributes: &wgpu::vertex_attr_array![0 => Float32x3],
+  };
+const VERT_AUX_LAYOUT: wgpu::VertexBufferLayout =
+  wgpu::VertexBufferLayout {
+    array_stride: std::mem::size_of::<VertexAux>() as wgpu::BufferAddress,
+    step_mode: wgpu::VertexStepMode::Vertex,
+    attributes: &wgpu::vertex_attr_array![1 => Float32x3],
+  };
+
+struct VertexBufferLayouts {
+  xs_layout: wgpu::VertexBufferLayout<'static>,
+  colors_layout: wgpu::VertexBufferLayout<'static>,
+}
+
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, Pod, Zeroable)]
+struct VertPushConstants {
+  model: Mat4,
+  view: Mat4,
+  proj: Mat4,
+}
+
+const OPENGL_TO_WGPU_MATRIX: Mat4 = Mat4::from_cols_array(&[
+  1.0, 0.0, 0.0, 0.0,
+  0.0, 1.0, 0.0, 0.0,
+  0.0, 0.0, 0.5, 0.5,
+  0.0, 0.0, 0.0, 1.0,
+]);
+
+struct Camera {
+  // view
+  eye: Vec3,
+  target: Vec3,
+  up: Vec3,
+  // proj
+  fovy: f32,
+  aspect: f32,
+  znear: f32,
+  zfar: f32,
+}
+impl Camera {
+  fn get_view(&self) -> Mat4 {
+    Mat4::look_at_rh(self.eye, self.target, self.up)
+  }
+  fn get_proj(&self) -> Mat4 {
+    OPENGL_TO_WGPU_MATRIX * Mat4::perspective_rh(self.fovy, self.aspect, self.znear, self.zfar)
+  }
+  // set aspect to match viewport size
+  fn set_aspect(&mut self, size: Size) {
+    self.aspect = (size.width as f32) / (size.height as f32);
+  }
+}
+impl Default for Camera {
+  fn default() -> Self {
+    let eye = Vec3::new(0.0, 2.0, 2.0);
+    let target = Vec3::new(0.0, 0.0, 0.0);
+    let up = Vec3::new(0.0, 0.0, 1.0);
+    let fovy = (45.0_f32).to_radians();
+    let aspect = 1.0;
+    let znear = 0.1;
+    let zfar = 10.0;
+    Self {
+      eye, target, up,
+      fovy, aspect, znear, zfar,
+    }
+  }
+}
+
+struct Mesh {
+  xs: Vec<VertexCore>,
+  colors: Vec<VertexAux>,
+  inds: Vec<u32>,
+
+  shift: Vec3,
+  rot: Quat,
+  scale: Vec3,
+
+  xs_buffer: Option<wgpu::Buffer>,
+  colors_buffer: Option<wgpu::Buffer>,
+  inds_buffer: Option<wgpu::Buffer>,
+}
+
+impl Mesh {
+  fn vert_buffer_layouts() -> VertexBufferLayouts {
+    VertexBufferLayouts {
+      xs_layout: VERT_CORE_LAYOUT,
+      colors_layout: VERT_AUX_LAYOUT,
+    }
+  }
+
+  fn inds_format() -> wgpu::IndexFormat {
+    wgpu::IndexFormat::Uint32
+  }
+
+  fn get_transform(&self) -> Mat4 {
+    Mat4::from_scale_rotation_translation(self.scale, self.rot, self.shift)
+  }
+}
+
+impl Default for Mesh {
+  fn default() -> Self {
+    Self {
+      xs: vec![],
+      colors: vec![],
+      inds: vec![],
+      shift: Vec3::ZERO,
+      rot: Quat::IDENTITY,
+      scale: Vec3::ONE,
+      xs_buffer: None,
+      colors_buffer: None,
+      inds_buffer: None,
+    }
+  }
+}
+
+struct GraphicsState {
   window: Arc<Window>,
   surface: wgpu::Surface<'static>,
   instance: wgpu::Instance,
@@ -22,7 +176,9 @@ struct State {
 
 #[derive(Default)]
 struct App {
-  state: Option<State>,
+  state: Option<GraphicsState>,
+  meshes: Vec<Mesh>,
+  camera: Camera,
 }
 
 impl App {
@@ -50,6 +206,11 @@ impl App {
     let enc_desc = wgpu::CommandEncoderDescriptor {
       label: Some("render encoder"),
     };
+    let mut push_consts = VertPushConstants {
+      proj: self.camera.get_proj(),
+      view: self.camera.get_view(),
+      model: Mat4::IDENTITY,
+    };
     let mut encoder = state.device.create_command_encoder(&enc_desc);
     {
       let color_attaches = [Some(wgpu::RenderPassColorAttachment {
@@ -71,15 +232,47 @@ impl App {
       };
       let mut render_pass = encoder.begin_render_pass(&rp_desc);
       render_pass.set_pipeline(&state.pipeline);
-      let verts = 0..3;
-      let insts = 0..1;
-      render_pass.draw(verts, insts);
+      self.meshes.iter().for_each(|mesh| {
+        render_pass.set_vertex_buffer(0, mesh.xs_buffer.as_ref().unwrap().slice(..));
+        render_pass.set_vertex_buffer(1, mesh.colors_buffer.as_ref().unwrap().slice(..));
+        render_pass.set_index_buffer(mesh.inds_buffer.as_ref().unwrap().slice(..), Mesh::inds_format());
+        push_consts.model = mesh.get_transform();
+        render_pass.set_push_constants(wgpu::ShaderStages::VERTEX, 0, bytemuck::bytes_of(&push_consts));
+        let inds = 0..(mesh.inds.len() as u32);
+        let vert_off = 0;
+        let insts = 0..1;
+        render_pass.draw_indexed(inds, vert_off, insts);
+      });
     }
 
     state.queue.submit(std::iter::once(encoder.finish()));
     output.present();
 
     Ok(())
+  }
+
+  fn init_mesh_buffers(&mut self) {
+    let state = &mut self.state.as_mut().unwrap();
+    self.meshes.iter_mut().for_each(|mesh| {
+      let buffer_desc = wgpu::util::BufferInitDescriptor {
+        label: Some("vertex buffer"),
+        contents: bytemuck::cast_slice(&mesh.xs),
+        usage: wgpu::BufferUsages::VERTEX,
+      };
+      mesh.xs_buffer = Some(state.device.create_buffer_init(&buffer_desc));
+      let buffer_desc = wgpu::util::BufferInitDescriptor {
+        label: Some("colors buffer"),
+        contents: bytemuck::cast_slice(&mesh.colors),
+        usage: wgpu::BufferUsages::VERTEX,
+      };
+      mesh.colors_buffer = Some(state.device.create_buffer_init(&buffer_desc));
+      let buffer_desc = wgpu::util::BufferInitDescriptor {
+        label: Some("inds buffer"),
+        contents: bytemuck::cast_slice(&mesh.inds),
+        usage: wgpu::BufferUsages::INDEX,
+      };
+      mesh.inds_buffer = Some(state.device.create_buffer_init(&buffer_desc));
+    });
   }
 }
 
@@ -98,13 +291,17 @@ impl ApplicationHandler for App {
       .enumerate_adapters(wgpu::Backends::all())
       .into_iter()
       .filter(|adapter| {
-        adapter.is_surface_supported(&surface)
+        adapter.is_surface_supported(&surface) &&
+          adapter.features().contains(wgpu::Features::PUSH_CONSTANTS)
       })
       .next().unwrap();
     let (device, queue) = adapter.request_device(
       &wgpu::DeviceDescriptor {
         required_features: wgpu::Features::empty(),
-        required_limits: wgpu::Limits::default(),
+        required_limits: wgpu::Limits {
+          max_push_constant_size: 128,
+          .. Default::default()
+        },
         label: None
       },
       None
@@ -133,7 +330,10 @@ impl ApplicationHandler for App {
     let pipeline_layout_desc = wgpu::PipelineLayoutDescriptor {
       label: Some("render pipeline layout"),
       bind_group_layouts: &[],
-      push_constant_ranges: &[],
+      push_constant_ranges: &[wgpu::PushConstantRange {
+        range: 0..(std::mem::size_of::<VertPushConstants>() as u32),
+        stages: wgpu::ShaderStages::VERTEX,
+      }],
     };
     let pipeline_layout = device.create_pipeline_layout(&pipeline_layout_desc);
     let frag_targets = [Some(wgpu::ColorTargetState {
@@ -147,8 +347,10 @@ impl ApplicationHandler for App {
       vertex: wgpu::VertexState {
         module: &shader,
         entry_point: "vs_main",
-        // todo: external vertex buffers
-        buffers: &[],
+        buffers: &[
+          Mesh::vert_buffer_layouts().xs_layout,
+          Mesh::vert_buffer_layouts().colors_layout,
+        ],
         // todo: push constants here?
         compilation_options: wgpu::PipelineCompilationOptions::default(),
       },
@@ -181,7 +383,7 @@ impl ApplicationHandler for App {
     // request initial draw
     window.request_redraw();
 
-    self.state = Some(State {
+    self.state = Some(GraphicsState {
       size,
       instance,
       queue,
@@ -191,6 +393,12 @@ impl ApplicationHandler for App {
       device,
       pipeline,
     });
+
+    // setup meshes
+    self.init_mesh_buffers();
+
+    // setup camera
+    self.camera.set_aspect(size);
   }
 
   fn window_event(
@@ -244,5 +452,25 @@ pub fn main() {
   let event_loop = EventLoop::new().unwrap();
   event_loop.set_control_flow(ControlFlow::Poll);
   let mut app = App::default();
+  app.meshes.push(Mesh {
+    xs: [
+      [0.5, -0.5, 0.0],
+      [-0.5, 0.5, 0.0],
+      [0.5, 0.5, 0.0],
+      [-0.5, -0.5, 0.0],
+    ].into_iter().map(VertexCore::new).collect(),
+    colors: [
+      [1.0, 1.0, 1.0],
+      [0.0, 1.0, 0.0],
+      [0.0, 0.0, 1.0],
+      [1.0, 0.0, 0.0],
+    ].into_iter().map(VertexAux::new).collect(),
+    inds: vec![
+      1, 0, 2,
+      0, 1, 3,
+    ],
+    .. Default::default()
+  });
+  app.camera = Default::default();
   event_loop.run_app(&mut app).unwrap();
 }
